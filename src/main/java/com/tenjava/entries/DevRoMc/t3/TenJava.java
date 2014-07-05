d3f99275f68d98279f0d77999277c0895a87c281
Erik Rosemberg <me@devrosemberg.com>
2014-07-05 10:32:17 -0300

    Edited the TenJava.java main class. (They asked for it)

--- src/main/java/com/tenjava/entries/DevRoMc/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/DevRoMc/t3/TenJava.java
@@ -2,4 +2,24 @@ package com.tenjava.entries.DevRoMc.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+    private static TenJava instance;
+
+    @Override
+    public void onEnable() {
+        setInstance(this);
+    }
+
+    @Override
+    public void onDisable() {
+        setInstance(null);
+    }
+
+    public static void setInstance(TenJava instance) {
+        TenJava.instance = instance;
+    }
+
+    public static TenJava getInstance() {
+        return instance;
+    }
+}
